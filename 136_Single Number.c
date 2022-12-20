30957a5d8906ee6cf9e708d277f6555674785e37
Byouree Lim <79687199+Byouree@users.noreply.github.com>
2022-12-20 11:03:00 -0800

    Update 136_Single Number.c

--- 136_Single Number.c
+++ 136_Single Number.c
@@ -1,3 +1,7 @@
+// nums = [2,2,1]
+// nums = 2^2^1= 0^1 = 1
+// nums = [4,1,2,1,2]
+// nums = 4^1^2^1^2 => 1^1^2^2^4 => 0^0^4 => 4
 int singleNumber(int* nums, int numsSize){
     int res = nums[0];
     for (int i=1; i < numsSize; i++){
